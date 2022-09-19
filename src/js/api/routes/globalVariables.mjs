78031bfa79a52e88d7c1d7c8e2c6ae2a34624120
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-09-19 20:42:17 +0200

    Imports and a script.js

---
+++ src/js/api/routes/globalVariables.mjs
@@ -0,0 +1,1 @@
+export const feedbackMsg = document.querySelector("#feedback");
--- src/js/api/validations/URLparams.mjs
+++ src/js/api/validations/URLparams.mjs
@@ -1,5 +1,5 @@
 // URLS for fetch
 export const BASE_URL = "https://nf-api.onrender.com/";
 export const statusUrl = "status/";
-export const registerUrl = "/api/v1/social/auth/register";
+export const registerUrl = "api/v1/social/auth/register";
 export const loginUrl = "api/v1/social/auth/login";
