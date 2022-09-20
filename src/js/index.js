5f8cefc9be921fba5120a7f57bd326081301d128
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-09-20 23:18:05 +0200

    Created logout function.

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,18 @@ import { checkUserAuth } from "./api/routes/authentication.mjs";
  * imported feature, checks if the user has valid access token or not.
  */
 checkUserAuth();
+
+// Gets logout button element by id.
+const logout = document.getElementById("logoutBtn");
+
+// Event listener for logging out.
+logout.addEventListener("click", logOut);
+
+/**
+ * User logout function.
+ * clears localStorage and triggers checkUserAuth()
+ */
+function logOut() {
+  localStorage.clear();
+  checkUserAuth();
+}
