a440e56315c5a000355a726c530fb91f41a3957f
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-10-01 19:59:15 +0200

    little cleanup

--- src/js/api/posts/posts.mjs
+++ src/js/api/posts/posts.mjs
@@ -1,4 +1,4 @@
-// IMPORTS
+// Imports
 import { checkUserAuth } from "../auth/userAuth.mjs";
 import * as create from "../helpers/postsElement.mjs";
 import * as storage from "../storage/user.mjs";
@@ -9,9 +9,6 @@ import * as response from "./responseFilter/filterResponse.mjs";
  * @param {string} url contains the string combination to retrieve posts.
  */
 export async function fetchPosts(url) {
-  /**
-   * imported feature, checks if the user has valid access token or not.
-   */
   checkUserAuth();
 
   try {
@@ -27,10 +24,7 @@ export async function fetchPosts(url) {
     const response = await fetch(url, postsData);
     const json = await response.json();
     const data = json;
-    console.log(data);
-    /**
-     * Creates posts on the post page.
-     */
+
     createPosts(data);
   } catch (error) {
     console.log(error);
@@ -42,7 +36,7 @@ export async function fetchPosts(url) {
  * @param {array} postArray contains the response from the API.
  */
 function createPosts(postArray) {
-  // container for posts.
+  // Container for posts.
   const container = document.getElementById("postsContainer");
 
   postArray.forEach((posts) => {
@@ -51,10 +45,10 @@ function createPosts(postArray) {
     const newDate = splittedDate[0];
     const dateCreated = newDate.replace("T", ", ");
 
-    // filters bad avatar images and returns placeholder image if there is none.
+    // Filters bad avatar images and returns placeholder image if there is none.
     let userAvatar = response.checkAvatar(posts.author.avatar);
 
-    // wrapper for my body content
+    // Wrapper for my body content
     const postsBodyContent = create.postWrapper();
 
     // Assembling post content
@@ -65,14 +59,14 @@ function createPosts(postArray) {
       create.postReactions(posts._count.comments, posts._count.reactions)
     );
 
-    // wrapper for all content
+    // Wrapper for all content
     const contentWrapper = document.createElement("div");
     contentWrapper.className = "card bg-primary border-0";
 
-    // wrapper for post content, header image goes outside of the body content.
+    // Wrapper for post content, header image goes outside of the body content.
     contentWrapper.append(create.postHeader(posts.media), postsBodyContent);
 
-    // post container because otherwise my layout gets destroyed. NOTE: might find another way around this.
+    // Post container because otherwise my layout gets destroyed. NOTE: might find another way around this.
     const post = document.createElement("div");
     post.className = "container bg-primary p-2 box d-flex flex-wrap mt-2";
     post.id = `${posts.id}`;
