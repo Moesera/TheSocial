70a631b177f4b7f7975cb2eb380e31a492769c4b
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-10-08 06:48:50 +0200

    Change to the url of the Delete function

--- src/js/api/posts/deletePost.mjs
+++ src/js/api/posts/deletePost.mjs
@@ -4,7 +4,7 @@ import { BASE_URL, postUrl } from "../helpers/constants.mjs";
 export async function deletePost(event) {
   const id = event.target.id;
 
-  const response = await fetch(`${BASE_URL}${postUrl}/${id}`, {
+  const response = await fetch(`${BASE_URL}${postUrl}${id}`, {
     method: "DELETE",
     headers: headers(),
   });
