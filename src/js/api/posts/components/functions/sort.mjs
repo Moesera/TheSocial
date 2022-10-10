6b4f04b0a2be8afe48a452c245be77643d18e758
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-10-10 05:53:09 +0200

    created sort functions

---
+++ src/js/api/posts/components/functions/sort.mjs
@@ -0,0 +1,45 @@
+import { arrayPosts } from "../../postsFeed.mjs";
+
+// Filter select container
+export const filterContainer = document.getElementById("filterSelect");
+export const filterStandard = document.getElementById("standardFilter");
+
+/**
+ * Sorts the array to newest post first, this is also the standard.
+ * @returns a sorted array with newest posts first
+ */
+export const newSort = () => {
+  const SortNewest = arrayPosts.sort((a, b) => {
+    if (a.created.toLowerCase() < b.created.toLowerCase()) return 1;
+    return -1;
+  });
+
+  return SortNewest;
+};
+
+/**
+ * Sorts the array title from A-Z
+ * @returns a sorted array with ascending alphabetical order by title
+ */
+export const titleAzSort = () => {
+  // const sortedPosts = apiArray.map((post) => post);
+  const byAzTitle = arrayPosts.sort((a, b) => {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+    return 1;
+  });
+
+  return byAzTitle;
+};
+
+/**
+ * Sorts the array title from Z-A.
+ * @returns a sorted array with descending alphabetical order by title
+ */
+export const titleZaSort = () => {
+  const byZaTitle = arrayPosts.sort((a, b) => {
+    if (a.title.toLowerCase() < b.title.toLowerCase()) return 1;
+    return -1;
+  });
+
+  return byZaTitle;
+};
