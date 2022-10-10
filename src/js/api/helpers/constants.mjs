811a35eb382e044aef4df612427df03fc4753966
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-10-10 19:45:13 +0200

    some changes to flags in the base url

--- src/js/api/helpers/constants.mjs
+++ src/js/api/helpers/constants.mjs
@@ -4,7 +4,7 @@ export const statusUrl = "/status";
 export const registerUrl = "/api/v1/social/auth/register";
 export const loginUrl = "/api/v1/social/auth/login";
 export const postUrl = "/api/v1/social/posts/";
-export const postsOption = "?_author=true&_comments=true&reactions=true";
+export const postsOption = "?_author=true&_comments=true&_reactions=true";
 export const profileUrl = `/api/v1/social/profiles`;
 export const profileOption = "?_posts=true&_following=true&_followers=true";
 // error message feedback container
