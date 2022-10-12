b518252beb330b703b9598a3b8ed16bda4db6339
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-10-12 17:00:01 +0200

    fixed page transition from register to login by successful account creation

--- src/js/api/services/register.mjs
+++ src/js/api/services/register.mjs
@@ -46,6 +46,9 @@ export function registerAccount(profile) {
                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
                                    </svg>
                                    <p class="mb-0"> Account created successfully </p>`;
+          setTimeout(() => {
+            location.replace("/src/pages/login/index.html");
+          }, 1500);
         }
       })
       .catch((error) => console.log("error", error));
