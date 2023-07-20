b2f831c2dd08d7bb1b7e85fbb8d47f29ae57e4be
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-07-20 13:19:47 +0200

    styling to post comments

--- src/js/api/posts/comments/index.mjs
+++ src/js/api/posts/comments/index.mjs
@@ -1,24 +1,24 @@
 export const postComments = (comments) => {
   console.log(comments);
-  
-const container = document.createElement("section");
 
-comments.forEach((comment) => {
-  const wrapper = document.createElement("div");
-  wrapper.className = "bg-primary w-100 p-2 g-col-6 rounded-1"
+  const container = document.createElement("section");
 
-  const author = document.createElement("p");
-  author.className = "mb-0 fst-italic fw-semibold"
-  author.innerText = `- ${comment.owner}`;
+  comments.forEach((comment) => {
+    const wrapper = document.createElement("div");
+    wrapper.className = "bg-primary w-100 p-2 g-col-6 rounded-1";
 
-  const body = document.createElement("p");
-  body.className = "bg-secondary p-1 mb-1 rounded-1";
-  body.innerText = comment.body;
+    const author = document.createElement("p");
+    author.className = "mb-0 fst-italic fw-semibold";
+    author.innerText = `- ${comment.owner}`;
 
-  wrapper.append(body, author);
+    const body = document.createElement("p");
+    body.className = "bg-secondary p-1 mb-1 rounded-1";
+    body.innerText = comment.body;
 
-  container.append(wrapper);
-})
+    wrapper.append(body, author);
 
-return container;
-}
+    container.append(wrapper);
+  });
+
+  return container;
+};
