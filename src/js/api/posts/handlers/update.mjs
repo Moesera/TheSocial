7b1fbc1458b4fc059e51340a205f54d60176a8bd
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-08-10 11:56:58 +0200

    removed creation of post form and input functions

--- src/js/api/posts/handlers/update.mjs
+++ src/js/api/posts/handlers/update.mjs
@@ -6,71 +6,17 @@ import { updatePostFetch } from "../updatePost.mjs";
  * @param {object} event Contains the event of the eventListener.
  * @returns An updated form with values from the post.
  */
-export const updatePostForm = (event) => {
-
-  // current post media values.
-  const postTitle = document.getElementById("editTitle");
-  const postTitleContent = document.getElementById("editTitle").textContent;
-  console.log(postTitle);
-
-  const postInput = createInput(postTitleContent, "text");
-  postInput.className = "card-text w-50 border-0 mb-1";
-  postTitle.replaceWith(postInput);
-
-  const postBody = document.getElementById("editBody");
-  const postBodyContent = document.getElementById("editBody").textContent;
-
-  const postTextarea = createTextArea(postBodyContent);
-  postTextarea.className = "w-100";
-  postBody.replaceWith(postTextarea);
-  
-  const media = event.target.closest(".card").querySelector(".card-img-top");
-  console.log(media);
-  const mediaContent = event.target.closest(".card").querySelector(".card-img-top").currentSrc;
-  const [postMedia, input] = createMediaInput(mediaContent);
-  input.className = "w-100 mt-1 border-0"
-  media.replaceWith(postMedia, input);
+export const updatePostForm = (event, title, body, mediaValue) => {
   // takes the id and place it in the url.
   const postId = event.target.id;
 
+  updatePost(title, body, mediaValue);
 
   // Adds post id to url without refreshing the page.
   window.history.replaceState(null, null, `?id=${postId}`);
 
   window.scrollTo(0, 0);
 };
-
-const createInput = (content, type) => {
-
-  const input = document.createElement("input");
-  input.value = content;
-  input.type = type;
-
-  return input;
-}
-
-const createTextArea = (content) => {
-  const textarea = document.createElement("textarea");
-
-  textarea.textContent = content;
-  textarea.className = "";
-
-  return textarea;
-}
-
-const createMediaInput = (content) => {
-
-  const img = document.createElement("img");
-  img.src = content;
-  img.className = "card-img-top";
-
-  const imgInput = createInput(content, "url");
-  imgInput.textContent = content;
-  imgInput.className = "";
-
-  return [img, imgInput];
-
-}
 
 /**
  * This is the post update cancel button function, it will update the site
@@ -94,17 +40,18 @@ export const cancelUpdate = () => {
  * @returns Form data constructed from the values of the form.
  * and then sent further to the ```updatePostFetch()```.
  */
-export const updatePost = (event) => {
-  event.preventDefault();
+export const updatePost = (title, body, media) => {
 
   const url = new URL(location.href);
   const id = url.searchParams.get("id");
 
-  const form = event.target;
+  const data = {
+    title, 
+    body, 
+    media
+  }
 
-  const formData = new FormData(form);
-  const formValues = Object.fromEntries(formData.entries());
-  formValues.id = id;
+  console.log(data)
 
-  updatePostFetch(formValues);
+  updatePostFetch(id, data);
 };
