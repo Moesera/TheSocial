d6742675f90cbc9a50555c17a9f8c0b59e8b27d4
Moesera <89355439+Moesera@users.noreply.github.com>
2023-09-12 18:40:49 +0200

    post like functionality

--- src/js/api/posts/components/reactions.mjs
+++ src/js/api/posts/components/reactions.mjs
@@ -1,6 +1,10 @@
-export const postReactions = (likes) => {
+import { BASE_URL, postUrl } from "../../helpers/constants.mjs";
+import { headers } from "../../auth/fetchAuth.mjs";
+
+export const postReactions = (likes, id) => {
   /** Like counter is set to one, if length does not show otherwise */
   let reactionCounter = 0;
+
   if (likes.length !== 0) {
     for (let y = 0; y < likes.length; y++) {
       reactionCounter += likes[y].count;
@@ -16,9 +20,30 @@ export const postReactions = (likes) => {
 
   const likeCounter = document.createElement("p");
   likeCounter.className = "m-0 ms-2";
-  likeCounter.textContent = reactionCounter;
+  likeCounter.innerHTML = reactionCounter;
+
+  likeIcon.addEventListener("click", () => {
+    postLike(1, "heart", id);
+    reactionCounter += 1;
+
+    likeCounter.innerHTML = reactionCounter;
+  });
 
   likeWrapper.append(likeIcon, likeCounter);
 
   return likeWrapper;
 };
+
+export async function postLike(count, symbol, id) {
+  const res = await fetch(`${BASE_URL}${postUrl}${id}/react/❤️`, {
+    method: "PUT",
+    body: JSON.stringify({
+      symbol: symbol,
+      count: count,
+      postId: id,
+    }),
+    headers: headers(),
+  });
+
+  return await res.json();
+}
