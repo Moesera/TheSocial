9d5144a1155d444a63e2aa2510d0212b07db6a30
Moesera <89355439+Moesera@users.noreply.github.com>
2023-09-12 22:04:32 +0200

    link btn and hover styles

--- src/js/api/profile/components/avatar.mjs
+++ src/js/api/profile/components/avatar.mjs
@@ -3,7 +3,7 @@ import { load } from "../../storage/user.mjs";
 export function avatarLink(container) {
   const avatar = document.createElement("img");
   avatar.style = "width: 2.3rem; height: 2.3rem; border-radius: 0.3rem;";
-  avatar.classList = "shadow link-border";
+  avatar.classList = "link-border link-btn link-hover";
   avatar.src = `${load("user").avatar}`;
 
   avatar.onerror = function () {
